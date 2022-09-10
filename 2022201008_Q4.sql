35a85213e33fce94f518bb0517d242eed1803c59
tarunkumar242000 <110759150+tarunkumar242000@users.noreply.github.com>
2022-09-10 21:33:22 +0530

    Delete 2022201008_Q4.sql

--- 2022201008_Q4.sql
+++
@@ -1,2 +0,0 @@
-select dnumber,count(*) as no_of_locations from DEPT_LOCATIONS where dnumber IN(select Dnumber from DEPARTMENT where mgr_ssn IN( select essn from DEPENDENT where sex="F" group by essn HAVING
-count(*)>=2 )) group by dnumber;
